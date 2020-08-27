a2046d103dd58cad5dedeb4d9ba8c7ddc29e415d
kzyanisara <kzyanisara@yahoo.com>
2020-08-27 12:25:00 +0700

    Basic js homework switchcase

--- Lab/swtichcase.js
+++ Lab/swtichcase.js
@@ -41,3 +41,35 @@ if (browser === 'Edge'){
 
 console.log('bye')
 
+// แปลง Code ดังกล่าวเป็น
+// switchcase statement
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+    alert( 0 );
+}
+if (a == 1) {
+    alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+    alert( '2,3' );
+}
+
+Ans
+let a = +prompt("a?", "");
+
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert("2,3");
+}
+
+
